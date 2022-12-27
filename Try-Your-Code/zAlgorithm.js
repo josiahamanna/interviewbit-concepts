99c7c09c9468137e270dbaeb100d3f11af60b9d2
Josiah Amanna <jnamanna@gmai.com>
2022-12-27 18:13:11 +0530

    Trees 2 (Level order)

--- Try-Your-Code/zAlgorithm.js
+++ Try-Your-Code/zAlgorithm.js
@@ -1,19 +1,74 @@
-const solve = (A) => {
+/*
+  Create Linked List
+*/
 
-  const test = (A) => {
-    if(A.length === 3) {
-      A.push(0);
-      return;
-    }
-    A.push(A[A.length-1] + 1);
-    test(A)
+let head = null;
+
+class Node {
+  constructor(x) {
+    this.data = x;
+    this.next = null;
+  }
+}
+
+let node1 = new Node(1);
+head = node1;
+let node2 = new Node(2);
+node1.next = node2;
+let node3 = new Node(2);
+node2.next = node3;
+let node4 = new Node(1);
+node3.next = node4;
+let node5 = new Node(1);
+node4.next = node5;
+
+/*
+  Find the kth element (k is 0 based index)
+  Ex: find 3rd index
+*/
+
+const solve = (A) => {
+  console.log(A);
+  let st = [],
+    count = 0,
+    temp = A;
+
+  while (temp != null) {
+    count++;
+    temp = temp.next;
   }
 
-  test(A)
+  temp = A;
 
-  return A;
+  if (count % 2 == 0) {
+    for (let i = 0; i < count; i++) {
+      if (i < count) {
+        st.push(temp.data);
+      } else if (st[st.length - 1] === temp.data) {
+        st.pop();
+      }
+      temp = temp.next;
+    }
+  } else {
+    for (let i = 0; i < count; i++) {
+      if (i < parseInt(count / 2)) {
+        st.push(temp.data);
+      } else if (i == parseInt(count / 2)) {
+      } else if (st[st.length - 1] == temp.data) {
+        console.log(st, temp.data);
+        st.pop();
+      }
+      temp = temp.next;
+    }
+  }
+
+  console.log(st, count);
+
+  if (st.length) {
+    return false;
+  } else {
+    return true;
+  }
 };
 
-const A = [];
-
-console.log(solve(A));
+console.log(solve(head));
