671783666f21cb6bd07f43f95e94898b770832d8
Josiah Amanna <jnamanna@gmai.com>
2022-12-30 19:42:04 +0530

    Trees (Cont.) Vertical order traversal

--- Try-Your-Code/zAlgorithm.js
+++ Try-Your-Code/zAlgorithm.js
@@ -1,74 +1,26 @@
-/*
-  Create Linked List
-*/
+const twoSum = (nums, target) => {
+	let hashMap = {}, result = [ ];
+	
+	for(let i = 0; i < nums.length; i++) {
+		console.log('hash map', hashMap)
+		console.log('i', i, 'nums[i]', nums[i])
+		console.log('target - nums[i]', target - nums[i], 'hashMap[target - nums[i]]', hashMap[target - nums[i]]);
+		if(hashMap[nums[i]] != undefined && hashMap[target - nums[i]] != undefined) {
+			console.log('hey I am in first if');
+			if(hashMap[target - nums[i]] != undefined) {
+				if(i != hashMap[target - nums[i]]) {
+					result.push(i);
+					result.push(hashMap[target - nums[i]]);
+					break;
+				}
+			}
+		} else {
+			hashMap[nums[i]] = i;
+		}
+		console.log('***')
+	}
 
-let head = null;
-
-class Node {
-  constructor(x) {
-    this.data = x;
-    this.next = null;
-  }
+	return result;
 }
 
-let node1 = new Node(1);
-head = node1;
-let node2 = new Node(2);
-node1.next = node2;
-let node3 = new Node(2);
-node2.next = node3;
-let node4 = new Node(1);
-node3.next = node4;
-let node5 = new Node(1);
-node4.next = node5;
-
-/*
-  Find the kth element (k is 0 based index)
-  Ex: find 3rd index
-*/
-
-const solve = (A) => {
-  console.log(A);
-  let st = [],
-    count = 0,
-    temp = A;
-
-  while (temp != null) {
-    count++;
-    temp = temp.next;
-  }
-
-  temp = A;
-
-  if (count % 2 == 0) {
-    for (let i = 0; i < count; i++) {
-      if (i < count) {
-        st.push(temp.data);
-      } else if (st[st.length - 1] === temp.data) {
-        st.pop();
-      }
-      temp = temp.next;
-    }
-  } else {
-    for (let i = 0; i < count; i++) {
-      if (i < parseInt(count / 2)) {
-        st.push(temp.data);
-      } else if (i == parseInt(count / 2)) {
-      } else if (st[st.length - 1] == temp.data) {
-        console.log(st, temp.data);
-        st.pop();
-      }
-      temp = temp.next;
-    }
-  }
-
-  console.log(st, count);
-
-  if (st.length) {
-    return false;
-  } else {
-    return true;
-  }
-};
-
-console.log(solve(head));
+console.log(twoSum([1,1,1,1,1,4,1,1,1,1,1,7,1,1,1,1,1], 11))
