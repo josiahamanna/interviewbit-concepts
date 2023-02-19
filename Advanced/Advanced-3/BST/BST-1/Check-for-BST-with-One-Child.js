98095f56200fa873922e78651428dee8c8f72db4
Josiah Amanna <josiah.amanna@nike.com>
2023-02-19 22:15:28 +0530

    Floor and Ceil of a node, Deleting a node

--- Advanced/Advanced-3/BST/BST-1/Check-for-BST-with-One-Child.js
+++ Advanced/Advanced-3/BST/BST-1/Check-for-BST-with-One-Child.js
@@ -6,7 +6,6 @@ If the current node is a leaf node (has no children), pop it from the stack and 
 If the current node is an internal node with a left child, add the current node to the stack and set the current node to its left child.
 If the current node is an internal node with a right child, return false (since each internal node in a BST with one child must have a left child).
 If the stack is empty, return true. Otherwise, return false.
-
 */
 
 function isValidBSTPreorder(preorder) {
